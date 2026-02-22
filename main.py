6b908c81b6c6dd449011d879dfaa567be1d9a2b2
smahraz <smahraz@student.1337.ma>
2026-02-22 17:10:08 +0000

    test: just a demo testing the bot

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
-def main():
-    print("Hello from chocolate!")
+from discord.ext import commands
+import discord
+import dotenv
+import os
+import logging
 
+
+dotenv.load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+handler = logging.FileHandler(
+    filename="log/discord.log",
+    encoding="utf-8",
+    mode="w"
+)
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Starting... {bot.user.name}")
+
+
+@bot.event
+async def on_member_join(members):
+    await members.send("Mr7babik")
+
+
+@bot.event
+async def on_message(msg: discord.Message):
+    if msg.author == bot.user:
+        return
+    if msg.content.lower().startswith("!delete "):
+        await msg.delete()
+        await msg.channel.send(
+            f"{msg.author.mention} your message was deleted"
+        )
+    await bot.process_commands(msg)
+
+@bot.command()
+async def ping(ctx: commands.Context):
+    await ctx.send(f"pong {ctx.author.mention}")
 
 if __name__ == "__main__":
-    main()
+    bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
