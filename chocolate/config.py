a57d65174b62c9f3d8e41188a2a142713cb60f22
smahraz <smahraz@student.1337.ma>
2026-02-24 17:14:15 +0000

    config: Add a config module
    - add moderators role to config.
    - enforce & load struct using pydantic.

---
+++ chocolate/config.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, Field, field_validator, ValidationError
+import json
+
+
+
+class BotConfig(BaseModel):
+    moderators: list[str] = Field(min_length=1)
+
+    @field_validator('moderators', mode="after")
+    @classmethod
+    def moderators_(cls, moderators: list[str]) -> list[str]:
+        for role in moderators:
+            if not 1 <= len(role) <= 100:
+                raise ValueError("Role name is so long")
+            if '\n' in role:
+                raise ValueError("Line breaks in role name")
+            if '\t' in role:
+                raise ValueError("Line breaks in role name")
+        return moderators
+
+
+try:
+    with open("config.json", 'r') as config_file:
+        bot_config = BotConfig.model_validate_json(config_file.read())
+except (FileNotFoundError, IsADirectoryError, PermissionError) as e:
+    print(f"{e.__class__.__name__}: Could not open config.json")
+    exit(1)
+
+except ValidationError:
+    print("ValidationError: parsing error")
+    exit(1)
+
+if __name__ == "__main__":
+    print(bot_config.moderators)
