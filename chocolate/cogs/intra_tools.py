c536aadb7e2a1fdf6213ada003153a32cde255a3
smahraz <smahraz@student.1337.ma>
2026-02-28 23:53:19 +0000

    fix(command): Required roles for `/profile`

--- chocolate/cogs/intra_tools.py
+++ chocolate/cogs/intra_tools.py
@@ -8,12 +8,33 @@ from discord import app_commands as app
 
 class IntraTools(commands.Cog):
 
+    async def cog_check(self, ctx: commands.Context) -> bool:
+        if not any(
+            role.name in bot_config.roles.intra_access
+            for role in ctx.author.roles
+        ):
+            raise commands.MissingAnyRole(bot_config.roles.intra_access)
+        return True
+
     @app.command(description="Show intra profile")
+    @app.describe(login="login you wanna show")
+    @app.checks.has_any_role(*bot_config.roles.intra_access)
     async def profile(self, interaction: Interaction, login: str):
         user_data = IntraV2.profile_info(login)
         await interaction.response.send_message(
             embed=ProfileCard.embed(user_data)
         )
+
+    @profile.error
+    async def profile_erro(self, interaction: Interaction, error):
+        if isinstance(error, app.MissingAnyRole):
+            await interaction.response.send_message(
+                "Sorry, you don't have permissions.\n"
+                "contact *_Admins_*",
+                ephemeral=True
+            )
+        else:
+            raise error
 
 
 async def setup(bot: commands.Bot):
