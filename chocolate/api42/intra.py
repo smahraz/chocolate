8a6311810e1df84f2b9d81a20734e3f9bf90c725
smahraz <smahraz@student.1337.ma>
2026-03-02 04:31:04 +0000

    feat(api): Api wrap -> last validated projects in a campus

--- chocolate/api42/intra.py
+++ chocolate/api42/intra.py
@@ -11,3 +11,27 @@ class IntraV2:
          /v2/users/:user_id
         """
         return ApiSession.get(URL_PREFIX + f"/v2/users/{login}")
+
+    @staticmethod
+    def validated_projects(
+            campuses: list[str],
+            markedat_from: str,
+            markedat_to: str = "now",
+            page_size: int = 100
+    ) -> dict:
+        """
+        /v2/projects_users
+        """
+
+        params = {
+            "range[marked_at]": f"{markedat_from},{markedat_to}",
+            "page[size]": f"{page_size}"
+        }
+
+        if campuses:
+            params.update({"filter[campus]": ",".join(campuses)})
+
+        return ApiSession.get(
+            URL_PREFIX + "/v2/projects_users",
+            params=params
+        )
